e37db66c498e1b840e59b9fc90cbc4ef41d0dcf9
azabroflovski <33807338+azabroflovski@users.noreply.github.com>
2018-12-29 16:51:37 +0500

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -22,3 +22,42 @@ var swiper = new Swiper('.swiper-container', {
 
 });
 
+
+
+var stickyOffset = $('#home').position().top + $('#home').outerHeight() - 20;
+
+$(window).scroll(function(){
+  var sticky = $('#header'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= stickyOffset) {
+    sticky.addClass('header-fixed');
+  } else {
+    sticky.removeClass('header-fixed');
+  }
+});
+
+$(".footer-to-top").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, 1200);
+  return false;
+});
+
+$(".home-next").click(function() {
+  $("html, body").animate({ scrollTop: $('#home').offset().top + $('#home').outerHeight() }, 1200);
+  return false;
+});
+
+
+var dataToCounter = 0;
+$("[data-to]").click(function(e) {
+
+    $("html, body").animate({ scrollTop: $(e.target.getAttribute('data-to')).position().top - $('#header').outerHeight() - 20}, 1200, function () {
+  
+    });
+    return false;
+  
+});
+
+
+
+
